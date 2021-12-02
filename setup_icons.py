f81e5e88087e6f9a5391c79be1fbf1eb8800e27b
ph04 <47356461+ph04@users.noreply.github.com>
2021-12-02 11:31:03 +0100

    Add files via upload

--- setup_icons.py
+++ setup_icons.py
@@ -1,23 +1,89 @@
+from typing import final
 from PIL import Image
 import requests
 from io import BytesIO
+import numpy as np
+import sys
 
-for i, pokemon_raw in enumerate(open("nameslist.txt").readlines()):
-    pokemon = pokemon_raw[:-1]
+def remove_horizontal_margins(rgba):
+    y = 0
 
-    url = "https://img.pokemondb.net/sprites/sword-shield/icon/" + pokemon + ".png"
+    while y < len(rgba):
+        if not any(map(lambda p: p[3], rgba[y])):
+            rgba.pop(y)
+
+            y -= 1
+
+        y += 1
+
+def build_new_image(png_idx_img):
+    # convert the image into RGBA format
+    rgba_img = png_idx_img.convert("RGBA")
+
+    # create the image array
+    rgba_img_arr = np.array(rgba_img)
+
+    # create a list of lists of lists from the array
+    rgba_img_list = rgba_img_arr.tolist()
+
+    # remove horizontal margins
+    remove_horizontal_margins(rgba_img_list)
+
+    # build the new RGBA image array from the modified list
+    new_rgba_img_arr = np.array(rgba_img_list, dtype=np.uint8)
+
+    # transpose the new RGBA array
+    new_rgba_img_arr_t = np.transpose(new_rgba_img_arr, (1, 0, 2))
+
+    # create a list of lists of lists from the transposed new array
+    new_rgba_img_list_t = new_rgba_img_arr_t.tolist()
+
+    # remove horizontal margins from the transposed array,
+    # so this action removes the vertical margins
+    remove_horizontal_margins(new_rgba_img_list_t)
+
+    # build the array of the new transposed list
+    final_rgba_img_arr_t = np.array(new_rgba_img_list_t, dtype=np.uint8)
+
+    # transpose the array again, to restore the image
+    final_rgba_img_arr = np.transpose(final_rgba_img_arr_t, (1, 0, 2))
+
+    # return the new image from the new RGBA array
+    return Image.fromarray(final_rgba_img_arr)
+
+def main():
+    upscale = 15 # defualt value
+
+    if len(sys.argv) > 1:
+        command = sys.argv[1] # this must exist
+
+        if command == "--upscale" or command == "-u":
+            try:
+                upscale = int(sys.argv[2])
+            except:
+                raise SyntaxError("Missing upscaling factor.")
+
+    for i, pokemon_raw in enumerate(open("nameslist.txt").readlines()):
+        # remove the '\n' a the end
+        pokemon = pokemon_raw[:-1]
     
-    response = requests.get(url)
-    img = Image.open(BytesIO(response.content))
+        # download the image in PNG indexed format
+        url = "https://img.pokemondb.net/sprites/sword-shield/icon/" + pokemon + ".png"
+        response = requests.get(url)
     
-    # this code is completely stolen from stackoverflow
-    # i was too lazy to even think about this
-    basewidth = 500
+        # load the image in PNG indexed format
+        png_idx_img = Image.open(BytesIO(response.content))
 
-    wpercent = (basewidth / float(img.size[0]))
-    hsize = int((float(img.size[1]) * float(wpercent)))
+        # return the new image from the new RGBA array
+        new_rgba_img = build_new_image(png_idx_img)
 
-    img = img.resize((basewidth, hsize), Image.ANTIALIAS)
-    img.save(f'/usr/local/opt/pokemon-icat/pokemon-icons/{pokemon}.png')
+        # upscale the RGBA image with the upscaling factor
+        img = new_rgba_img.resize((new_rgba_img.width * upscale, new_rgba_img.height * upscale), Image.BOX)
 
-    print(f"{pokemon} saved! [{i + 1}/898]")
+        # save the processed RGBA image
+        img.save(f"pokemon-icons/{pokemon}.png")
+    
+        print(f"{pokemon} saved! [{i + 1}/898]")
+
+if __name__ == "__main__":
+    main()

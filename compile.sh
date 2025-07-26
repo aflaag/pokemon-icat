f8717d94f787941c7d41bf9aeef221bd631526b0
aflaag <alessio.bandiera02@gmail.com>
2025-07-26 22:26:54 +0200

    removed comments

--- compile.sh
+++ compile.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 
-# Chiede la password sudo all'inizio
 sudo -v
 
-# Mantiene attiva la sessione sudo fino alla fine dello script
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 sudo cp target/release/pokemon-icat /usr/bin/

52d6c928e3e53fd721fcac9b8de266fcfce72a0c
aflaag <alessio.bandiera02@gmail.com>
2025-07-27 23:29:48 +0200

    fixed uninstall.sh

--- bin/uninstall.sh
+++ bin/uninstall.sh
@@ -5,6 +5,6 @@ sudo -v
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 sudo rm -f /usr/bin/pokemon-icat
-rm -rf $HOME/.cache/pokemon-icat
+rm -rf "$HOME/.local/share/pokemon-icat"
 
 echo "pokemon-icat was successfully uninstalled! :("

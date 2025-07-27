2710908cce4e67e1b82e45729f62bb3dae591f59
aflaag <alessio.bandiera02@gmail.com>
2025-07-27 16:02:16 +0200

    this wasn't the problem

--- default.nix
+++ default.nix
@@ -29,9 +29,9 @@ let
 
         outputHashMode = "recursive";
         outputHashAlgo = "sha256";
-        # outputHash = "sha256-RKve62/khQMo71pYzefiEhi2vIde/r3bNslLhs/00rk=";
+        outputHash = "sha256-RKve62/khQMo71pYzefiEhi2vIde/r3bNslLhs/00rk=";
         # outputHash = "sha256-1TnfKN8Ij+pbK6vLXdvbV1qud2HfDHeIJQTzTK+jJP0=";
-        outputHash = "sha256-hOWfpeQz0or/2G9VzYnuc6AFHvlsS5NjmQmMOC01jFM=";
+        # outputHash = "sha256-hOWfpeQz0or/2G9VzYnuc6AFHvlsS5NjmQmMOC01jFM=";
 
         src = ./.;
         # src = src;
